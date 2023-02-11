aa3b39e40e81d579dbe071e6961fe5be7d4239db
ReneRisa <risael31@gmail.com>
2023-02-11 14:22:28 -0500

    Create countUp() method

--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -27,5 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         mCount++;
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
     }
 }
