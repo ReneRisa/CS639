279caafa36ee0ae3c3c5d51bdd12790b66e1f742
ReneRisa <risael31@gmail.com>
2023-02-18 10:54:18 -0500

    Pushing new project Scrolling Text
    Pushing new project Scrolling Text

--- HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,23 +1,36 @@
 package com.example.hellotoast;
 
+import static android.graphics.Color.BLUE;
+import static android.graphics.Color.GRAY;
+import static android.graphics.Color.GREEN;
+import static android.graphics.Color.YELLOW;
+import static android.graphics.Color.rgb;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int mCount = 0;
+    private int mCount;
     private TextView mShowCount;
 
+    private Button buttonCountZero;
+    private Button buttonCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //to find a view(textview in this case) in the layout by id
         mShowCount = (TextView) findViewById(R.id.show_count);
+        buttonCountZero = findViewById(R.id.button_zero);
+        buttonCount = findViewById(R.id.button_count);
     }
 
     public void showToast(View view) {
@@ -27,8 +40,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         mCount++;
+        buttonCountZero.setBackgroundColor(YELLOW);
         if(mShowCount != null){
             mShowCount.setText(Integer.toString(mCount));
+        }
+        if(mCount%2==0){
+            view.setBackgroundColor(rgb(200, 1, 20));
+        }else{
+            view.setBackgroundColor(GREEN);
+        }
+    }
+
+    public void countZero(View view) {
+        Log.i("MainActivity","pressed zero button");
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(0));
+            mCount = 0;
+            buttonCountZero.setBackgroundColor(GRAY);
+            buttonCount.setBackgroundColor(BLUE);
         }
     }
 }
