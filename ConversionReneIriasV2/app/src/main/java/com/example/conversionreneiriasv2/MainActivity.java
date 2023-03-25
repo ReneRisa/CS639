293e201f16ef39344a2215e1a0e215fcefeaae2c
ReneRisa <risael31@gmail.com>
2023-03-25 09:56:43 -0400

    Updating project

--- ConversionReneIriasV2/app/src/main/java/com/example/conversionreneiriasv2/MainActivity.java
+++ ConversionReneIriasV2/app/src/main/java/com/example/conversionreneiriasv2/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.conversionreneiriasv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.conversionreneiriasv2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+    public void currencyConvert(View view) {
+        double usd = Double.parseDouble(binding.editTextUSD.getText().toString());
+        double eur = usd * 0.88;
+        binding.editTextEURO.setText(String.valueOf(eur));
     }
 }
