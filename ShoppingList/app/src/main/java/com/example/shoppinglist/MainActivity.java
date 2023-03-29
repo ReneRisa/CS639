934e693a6ef68013ad49e70fbbda965a86cc47ee
ReneRisa <risael31@gmail.com>
2023-03-29 17:09:04 -0400

    Adding RecyclerView

--- ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -60,6 +60,14 @@ public class MainActivity extends AppCompatActivity {
                         i++;
                         break;
                 }
+                //Get a handle to the RecyclerViewer
+                mRecyclerView = binding.recyclerview;
+                //create an adapter and supply the data to be displayed
+                mAdapter = new WordListAdapter(this, mWordList);
+                //Connect the adapter with the RecyclerView
+                mRecyclerView.setAdapter(mAdapter);
+                // Give the RecyclerView  a default layout manager
+                mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
                 //int i = 1;
                 //while(i < 11){
                     //if(binding.textView2.getText().toString() == ""){
